d21338572b5a90676b4f8f7981bf1a202d48030d
Charles Lanfear <cclanfear@gmail.com>
2022-10-17 13:45:14 +0100

    Quick fix

--- _lectures/2_distributions-relationships/slides_distributions-relationships.R
+++ _lectures/2_distributions-relationships/slides_distributions-relationships.R
@@ -51,5 +51,19 @@ shootings |> slice(c(1, 5, 9))
 ##     filter(armed == "unarmed" |
 ##           age < 18)
 
-unarmed <- shootings |> filter(armed == "unarmed")
-head(unarmed, 4)
+metro_2021 <- read_csv("https://clanfear.github.io/ioc_iqa/_data/metro_2021.csv")
+
+glimpse(metro_2021)
+
+westminster <- metro_2021 |> filter(borough == "Westminster")
+head(westminster, 4)
+
+## plot(robbery ~ month,
+##      data = westminster,
+##      xlab = "Month",
+##      ylab = "Robbery",
+##      main = "Robbery in Westminster in 2021",
+##      col = "red",
+##      cex.lab = 1.5,
+##      cex.main= 1.5,
+##      pch = 16)
