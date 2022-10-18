470de4ddf3044181d729e7ff4988172b5e69e243
Charles Lanfear <cclanfear@gmail.com>
2022-10-18 14:33:16 +0100

    Create create_communities.R

---
+++ _lectures/2_distributions-relationships/syntax/create_communities.R
@@ -0,0 +1,10 @@
+communities <- tibble(Area = rep(c("Rural", "Urban"), each = 5000),
+                `Pop Density` = c(rnorm(5000, 10, 2),
+                                  rnorm(5000, 20, 3)),
+                `Crime Rate` = (rpois(10000, `Pop Density`)^2) * 5000 / 100000) |>
+  mutate(Disadvantage = 0.3*scale(`Crime Rate`) - 0.1*scale(`Pop Density`) + rnorm(5000)) |>
+  mutate(Incarceration = 0.3*scale(`Crime Rate`) + 0.2*scale(`Pop Density`) + 0.2*scale(Disadvantage) + rnorm(5000)) |>
+  mutate(Disadvantage = factor(ntile(Disadvantage, 3), labels = c("Low", "Medium", "High")),
+         Incarceration = factor(ntile(Incarceration, 3), labels = c("Low", "Medium", "High"))) |>
+  sample_n(300)
+save(communities, file = "./_data/communities.csv")
