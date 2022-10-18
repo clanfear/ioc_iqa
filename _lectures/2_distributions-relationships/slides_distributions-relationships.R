469a41570b9c3ee73cabba859f98df8f75142365
Charles Lanfear <cclanfear@gmail.com>
2022-10-18 16:55:03 +0100

    SLide update

--- _lectures/2_distributions-relationships/slides_distributions-relationships.R
+++ _lectures/2_distributions-relationships/slides_distributions-relationships.R
@@ -23,6 +23,7 @@ is.na(vector_w_missing)
 mean(vector_w_missing[!is.na(vector_w_missing)]) #<<
 
 library(readr)
+library(ggplot2)
 library(dplyr)
 
 communities <- 
@@ -30,36 +31,116 @@ communities <-
     "https://clanfear.github.io/ioc_iqa/_data/communities.csv"
     )
 
-
 glimpse(communities)
 
-sort(table(communities$Incarceration))
+proportions(table(communities$disadvantage))
 
-communities |> pull(Incarceration) |> table() |> sort()
+communities |> pull(disadvantage) |> table() |> proportions()
 
-communities <- communities |>
-  mutate(Disadvantage = 
-           factor(Disadvantage, 
-                  levels = c("Low", "Medium", "High")))
+communities |>
+  mutate(high_crime = crime_rate > mean(crime_rate)) |>
+  head(4)
 
-shootings |> filter(armed == "unarmed") |> head()
+communities <- communities |> #<<
+  mutate(disadvantage = 
+           factor(disadvantage, levels = c("Low", "Medium", "High")), #<<
+         incarceration = 
+           factor(incarceration, levels = c("Low", "Medium", "High"))
+         )
 
-shootings |> slice(c(1, 5, 9))
+communities |> pull(disadvantage) |> table()
 
-## shootings |>
-##     filter(armed == "unarmed" &
-##              age < 18)
+communities |> pull(incarceration) |> table()
 
-## shootings |>
-##     filter(armed == "unarmed" |
-##           age < 18)
+communities |> filter(incarceration == "High") |> head()
 
-glimpse(communities)
+## communities |>
+##   filter(disadvantage == "Low" &
+##          crime_rate > 40)
 
-communities |> count(Incarceration)
+## communities |>
+##   filter(disadvantage == "Low" |
+##          crime_rate > 40)
 
+communities |> count(incarceration)
 
-communities %>% group_by(Area) %>% summarize(`Mean Crime Rate` = round(mean(`Crime Rate`), 2), 
-                                       `Std. Dev.` = round(sd(`Crime Rate`),2 ),
-                                       N = n()) %>%
-  knitr::kable()
+communities |> count(incarceration) |> mutate(proportion = n/sum(n))
+
+## install.packages("janitor")
+
+library(janitor)
+
+communities |> tabyl(incarceration)
+
+communities |>
+  summarize(mean_crime_rate = mean(crime_rate),
+            sd_crime_rate = sd(crime_rate), 
+            n = n())
+
+communities |> tabyl(disadvantage, incarceration)
+
+communities |> tabyl(disadvantage, incarceration) |>
+  adorn_percentages() # converts to cell percentages #<<
+
+communities |> 
+  tabyl(disadvantage, incarceration) # make table
+
+communities |> 
+  tabyl(disadvantage, incarceration) |> # make table
+  adorn_totals(c("row", "col")) # add row/col totals
+
+communities |> 
+  tabyl(disadvantage, incarceration) |> # make table
+  adorn_totals(c("row", "col")) |> # add row/col totals
+  adorn_percentages()# make cells percentages
+
+communities |> 
+  tabyl(disadvantage, incarceration) |> # make table
+  adorn_totals(c("row", "col")) |> # add row/col totals
+  adorn_percentages() |> # make cells percentages
+  adorn_pct_formatting(digits = 2) # round to 2 digits
+
+communities |> 
+  tabyl(disadvantage, incarceration) |> # make table
+  adorn_totals(c("row", "col")) |> # add row/col totals
+  adorn_percentages() |> # make cells percentages
+  adorn_pct_formatting(digits = 2) |> # round to 2 digits
+  adorn_ns() # add counts in parentheses
+
+communities |> 
+  tabyl(disadvantage, incarceration) |> # make table
+  adorn_totals(c("row", "col")) |> # add row/col totals
+  adorn_percentages() |> # make cells percentages
+  adorn_pct_formatting(digits = 2) |> # round to 2 digits
+  adorn_ns() |> # add counts in parentheses
+  adorn_title() # add col variable name
+
+communities |>
+  filter(disadvantage=="High") |> #<<
+  summarize(
+   mean_crime = mean(crime_rate),
+   sd_crime   = sd(crime_rate), 
+   n          = n())
+
+communities |>
+  filter(disadvantage=="Low") |> #<<
+  summarize(
+   mean_crime = mean(crime_rate),
+   sd_crime   = sd(crime_rate), 
+   n          = n())
+
+communities |>
+  group_by(disadvantage) |> #<<
+  summarize(mean_crime = mean(crime_rate),
+            sd_crime = sd(crime_rate), 
+            n = n())
+
+cor(communities$pop_density, communities$crime_rate)
+
+with(communities, cor(pop_density, crime_rate))
+
+communities |> summarize(R = cor(pop_density, crime_rate))
+
+communities |>
+  group_by(disadvantage) |>
+  summarize(R = cor(pop_density, crime_rate))
--- _lectures/2_distributions-relationships/syntax/create_communities.R
+++ _lectures/2_distributions-relationships/syntax/create_communities.R
@@ -1,7 +1,7 @@
 communities <- tibble(Area = rep(c("Rural", "Urban"), each = 5000),
                 `Pop Density` = c(rnorm(5000, 10, 2),
                                   rnorm(5000, 20, 3)),
-                `Crime Rate` = (rpois(10000, `Pop Density`)^2) * 5000 / 100000) |>
+                `Crime Rate` = (rpois(10000, `Pop Density`)^2) * round(rnorm(10, 10000, 500)) / 100000) |>
   mutate(Disadvantage = 0.3*scale(`Crime Rate`) - 0.1*scale(`Pop Density`) + rnorm(5000)) |>
   mutate(Incarceration = 0.3*scale(`Crime Rate`) + 0.2*scale(`Pop Density`) + 0.2*scale(Disadvantage) + rnorm(5000)) |>
   mutate(Disadvantage = factor(ntile(Disadvantage, 3), labels = c("Low", "Medium", "High")),
@@ -9,3 +9,5 @@ communities <- tibble(Area = rep(c("Rural", "Urban"), each = 5000),
   sample_n(300) |>
   janitor::clean_names()
 write_csv(communities, file = "_data/communities.csv")
+
+communities |> filter(disadvantage == "Low" & crime_rate < 3)
