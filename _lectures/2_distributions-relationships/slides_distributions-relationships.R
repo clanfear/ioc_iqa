09ab03f18b92157eeb4be9f49f6dd545759355da
Charles Lanfear <cclanfear@gmail.com>
2022-10-18 15:04:59 +0100

    Lowercase data names

--- _lectures/2_distributions-relationships/slides_distributions-relationships.R
+++ _lectures/2_distributions-relationships/slides_distributions-relationships.R
@@ -25,17 +25,22 @@ mean(vector_w_missing[!is.na(vector_w_missing)]) #<<
 library(readr)
 library(dplyr)
 
-shootings <- 
-  read_csv( 
-    "https://clanfear.github.io/ioc_iqa/_data/fatal-police-shootings-data.csv"
+communities <- 
+  read_csv(
+    "https://clanfear.github.io/ioc_iqa/_data/communities.csv"
     )
 
 
-glimpse(shootings)
+glimpse(communities)
 
-sort(table(shootings$race))
+sort(table(communities$Incarceration))
 
-shootings$race |> table() |> sort()
+communities |> pull(Incarceration) |> table() |> sort()
+
+communities <- communities |>
+  mutate(Disadvantage = 
+           factor(Disadvantage, 
+                  levels = c("Low", "Medium", "High")))
 
 shootings |> filter(armed == "unarmed") |> head()
 
@@ -49,187 +54,12 @@ shootings |> slice(c(1, 5, 9))
 ##     filter(armed == "unarmed" |
 ##           age < 18)
 
-metro_2021 <- read_csv("https://clanfear.github.io/ioc_iqa/_data/metro_2021.csv")
+glimpse(communities)
 
-glimpse(metro_2021)
+communities |> count(Incarceration)
 
-metro_2021 |> 
-  distinct(borough) |>
-  head()
 
-metro_2021 |> 
-  distinct(subregion, borough) |>
-  count(subregion)
-
-metro_2021 |> 
-  distinct(subregion, borough) |>
-  count(subregion) |>
-  mutate(proportion = n/sum(n))
-
-metro_2021 |> 
-  distinct(borough, subregion, deprivation) |>
-  count(deprivation, subregion) |>
-  head()
-
-metro_2021 |> 
-  filter(month == "2021-06-01") |>
-  summarise(n_obs = n(),
-            mean_robbery = mean(robbery),
-            sd_robbery = sd(robbery))
-
-metro_2021 |> 
-  filter(borough == "Westminster") |>
-  summarise(n_obs = n(),
-            median_burglary = median(burglary),
-            sum_burglary = sum(burglary))
-
-metro_2021 |>
-  group_by(month) |> #<<
-    summarise(n_obs = n(),
-            mean_robbery = mean(robbery),
-            sd_robbery = sd(robbery)) |>
-    head(5)
-
-metro_2021 |> 
-  group_by(borough) |> #<<
-  summarise(n_obs = n(),
-            median_burglary = median(burglary),
-            sum_burglary = sum(burglary)) |>
-  head()
-
-westminster <- metro_2021 |> filter(borough == "Westminster")
-head(westminster, 4)
-
-plot(robbery ~ month, 
-     data = westminster, 
-     xlab = "Month", 
-     ylab = "Robbery",
-     main = 
-       "Robbery in Westminster", 
-     col = "red", 
-     cex.lab = 1.5,
-     cex.main= 1.5,
-     pch = 16)
-
-library(ggplot2)
-
-ggplot(data = westminster, 
-        aes(x = month, y = robbery)) +
-  geom_point()
-
-ggplot(data = westminster,  #<<
-       aes(x = month, y = robbery)) #<<
-
-ggplot(data = westminster, 
-      aes(x = month, y = robbery)) +
-    geom_point() #<<
-
-ggplot(data = westminster, 
-      aes(x = month, y = robbery)) +
-  geom_point(color = "red", size = 3) #<<
-
-ggplot(data = westminster, 
-      aes(x = month, y = robbery)) +
-  geom_point(color = "red", size = 3) +
-  labs(x = "Month") #<<
-
-ggplot(data = westminster, 
-      aes(x = month, y = robbery)) +
-  geom_point(color = "red", size = 3) +
-  labs(x = "Month",
-       y = "Robbery") #<<
-
-ggplot(data = westminster, 
-      aes(x = month, y = robbery)) +
-  geom_point(color = "red", size = 3) +
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in Westminster") #<<
-
-ggplot(data = westminster, 
-      aes(x = month, y = robbery)) +
-  geom_point(color = "red", size = 3) +
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in Westminster") +
-  theme_bw() #<<
-
- ggplot(data = westminster, 
-      aes(x = month, y = robbery)) +
-  geom_point(color = "red", size = 3) +
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in Westminster") +
-   theme_bw(base_size=18) #<<
-
- ggplot(data = metro_2021, 
-      aes(x = month, y = robbery)) +
-  geom_point(color = "red", size = 3) +
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in London") +
-   theme_bw(base_size=18) #<<
-
- ggplot(data = metro_2021, 
-      aes(x = month, y = robbery)) +
-  geom_line(color = "red", size = 3) + #<<
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in London") +
-  theme_bw(base_size=18)
-
- ggplot(data = metro_2021, 
-      aes(x = month, y = robbery, group = borough)) +
-  geom_line(color = "red", size = 3) + #<<
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in London") +
-  theme_bw(base_size=18)
-
- ggplot(data = metro_2021, 
-      aes(x = month, y = robbery, group = borough)) +
-  geom_line(color = "red") + #<<
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in London") +
-  theme_bw(base_size=18)
-
- ggplot(data = metro_2021, 
-      aes(x = month, y = robbery, group = borough,
-          color = subregion)) + #<<
-  geom_line() + 
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in London") +
-  theme_bw(base_size=18)
-
- ggplot(data = metro_2021, 
-      aes(x = month, y = robbery, group = borough,
-          color = subregion)) + #<<
-  geom_line() + 
-  facet_wrap(~subregion) +
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in London") +
-  theme_bw(base_size=18)
-
- ggplot(data = metro_2021, 
-      aes(x = month, y = robbery, group = borough,
-          color = subregion)) + #<<
-  geom_line() + 
-  facet_wrap(~subregion) +
-  labs(
-    x = "Month",
-    y = "Robbery",
-    title= "Robbery in London") +
-  theme_bw(base_size=18) +
-  theme(legend.position = "none")
+communities %>% group_by(Area) %>% summarize(`Mean Crime Rate` = round(mean(`Crime Rate`), 2), 
+                                       `Std. Dev.` = round(sd(`Crime Rate`),2 ),
+                                       N = n()) %>%
+  knitr::kable()
--- _lectures/2_distributions-relationships/syntax/create_communities.R
+++ _lectures/2_distributions-relationships/syntax/create_communities.R
@@ -6,5 +6,6 @@ communities <- tibble(Area = rep(c("Rural", "Urban"), each = 5000),
   mutate(Incarceration = 0.3*scale(`Crime Rate`) + 0.2*scale(`Pop Density`) + 0.2*scale(Disadvantage) + rnorm(5000)) |>
   mutate(Disadvantage = factor(ntile(Disadvantage, 3), labels = c("Low", "Medium", "High")),
          Incarceration = factor(ntile(Incarceration, 3), labels = c("Low", "Medium", "High"))) |>
-  sample_n(300)
+  sample_n(300) |>
+  janitor::clean_names()
 write_csv(communities, file = "_data/communities.csv")
