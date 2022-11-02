852a692b26c1a37208881b45f2f80c13e893feda
Charles Lanfear <cclanfear@gmail.com>
2022-11-02 16:30:17 +0000

    street_crime

--- _assignments/create_robbery.R
+++ _assignments/create_robbery.R
@@ -22,20 +22,14 @@ street_crime <- tibble(
            3  * as.numeric(crime_type=="Assault") + 
            2 * sex_num + 
            (age/4),
-         sentence = rnbinom(n(), mean_months, 0.7))
+         sentence = rnbinom(n(), mean_months, 0.6))
 
+write_csv(street_crime, file = "_data/street_crime.csv")
 
 lm(sentence ~ sex + age + crime_type, data = street_crime) |> summary()
 
 ggplot(street_crime, aes(x = age, y = sentence)) + geom_point() + geom_smooth(method = "lm")
 
-street_crime |> janitor::tabyl(sex, crime_type)
-
-sample(c("Robbery", "Theft", "Assault"), size = sample_size, prob = c(0.1 + 0.2*sex_num, 
-                                                                      0.5 - 0.1*sex_num, 
-                                                                      0.2 + 0.3*sex_num ), replace=TRUE)
-
-sample(c("Robbery", "Theft", "Assault"), size = sample_size, replace= TRUE, prob = c(0.1, 0.5, 0.4))
-rpois(10, 2)
+street_crime |> janitor::tabyl(sex, crime_type) |> janitor::chisq.test()
 
 
