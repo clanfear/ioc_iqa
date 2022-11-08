202b6f1df37051c8cbea50bfb7cf4544fc5c18a9
clanfear <cclanfear@gmail.com>
2022-11-08 22:49:12 +0000

    Slides

--- _lectures/5_linear-models-ii/slides_linear-models-ii.R
+++ _lectures/5_linear-models-ii/slides_linear-models-ii.R
@@ -5,6 +5,10 @@ communities <-
   mutate(across(c(incarceration, disadvantage), 
                 ~ factor(., levels = c("Low", "Medium", "High"))))
 
+x <- round(runif(612, -0.49, 9.49),0)
+cat(x, fill = TRUE)
+
+
 s_n <- 10000
 po_data <- tibble(
   bd = runif(s_n, 0, 1), # Random uniform variable
@@ -41,11 +45,26 @@ lm(yt ~ treat, data = po_data) |>
 
 ## ?tidy
 
-## ?tidy.lm
+## ?tidy.lm # tidy() method for lm objects
 
-## ?summary.lm # Method for lm objects
+## ?summary.lm # summary method for lm objects
 
 
+
+lm(crime_rate ~ pop_density,
+   data = communities) |>
+  augment() |> #<<
+  slice(1) |> #<<
+  select(.fitted, pop_density)
+
+ggplot(communities,
+       aes(x = pop_density, 
+           y = crime_rate)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal() + 
+  geom_hline(yintercept = 35.43, color = "red") +
+  geom_vline(xintercept = 18.17, color = "red")
 
 ex_lm <- lm(crime_rate ~ pop_density + disadvantage, 
             data = communities)
@@ -53,11 +72,11 @@ fitted(ex_lm) |> head(4) # Returns them as a vector
 
 augment(ex_lm) |> head(4) # Returns them added to original data
 
-ex_lm |> 
+ex_lm |> # getting coefficients
   tidy() |>
   select(term, estimate)
 
-communities[1,c(2, 4)]
+communities[1, c(2, 4)]
 fitted(ex_lm)[1]
 
 predict(ex_lm, newdata = data.frame(pop_density = 10, 
@@ -71,7 +90,7 @@ communities |> pull(pop_density) |> min()
 lm_1 <- lm(crime_rate ~ disadvantage, data = communities)
 glance(lm_1)
 
-lm_2 <- lm(crime_rate ~ disadvantage + pop_density, data = communities)
+lm_2 <- lm(crime_rate ~ disadvantage + pop_density, data =communities)
 lm_3 <- lm(crime_rate ~ disadvantage + pop_density + area, 
            data = communities)
 rbind(glance(lm_1), # rbind() stacks output as rows #<<
@@ -80,13 +99,12 @@ rbind(glance(lm_1), # rbind() stacks output as rows #<<
 
 cor_mat <- matrix(c(1, 0.9, 0.3, 0.9, 1, 0.3, 0.3, 0.3, 1), nrow = 3)
 ex_data <- MASS::mvrnorm(n = 250, rep(0, 3), cor_mat) |>
-  data.frame() |> setNames(c("x", "z", "y"))
-cor(ex_data)
+  data.frame() |> setNames(c("x", "z", "y")) # add column names
+cor(ex_data) # correlation matrix
+
 lm(y ~ x + z, data = ex_data) |> tidy()
 
-rbind(glance(lm_1),
-      glance(lm_2), 
-      glance(lm_3)) |> 
+rbind(glance(lm_1), glance(lm_2), glance(lm_3)) |> 
   select(r.squared, adj.r.squared)
 
 anova(lm_2, lm_3)
