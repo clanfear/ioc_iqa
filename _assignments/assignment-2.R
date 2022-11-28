a10dc0180c7a2f36e908028d8ba349508fc2421c
Charles Lanfear <cclanfear@gmail.com>
2022-11-28 15:41:01 +0000

    Assignment 2 data

---
+++ _assignments/assignment-2.R
@@ -0,0 +1,18 @@
+# Your Name
+# Email
+# Assessment 2
+
+# Setup 
+
+# Load packages and data in this section
+
+# 1
+# 1.1
+
+# replace this with code for 1.1 and so on
+
+# If you struggled with something and couldn't quite get it, make a note of it using comments like this. Credit given for working at it!
+
+# 1.2
+
+# 1.3
---
+++ _assignments/create_neighb_crime.R
@@ -0,0 +1,36 @@
+# Log outcome on homicide, no zeroes
+# Quadratic disadvantage
+# Interaction between control and physical opportunity
+# Dis / CE -> opp
+set.seed(49)
+library(tidyverse)
+standardize <- function(x){
+  return((x - mean(x))/sd(x))
+}
+sample_size <- 343
+neighb_crime <- 
+  MASS::mvrnorm(sample_size, mu = c(0,0, 0), Sigma  = matrix(c(1, 0.3, 0.2, 
+                                                            0.3, 1, 0.2, 
+                                                            0.2, 0.3, 1), nrow =3)) |>
+  data.frame() |>
+  setNames(c("disadvantage", "instability", "heterogeneity")) |>
+  mutate(across(c(disadvantage, instability, heterogeneity), ~standardize(.)),
+         control = standardize(-0.3*disadvantage -0.15*instability -0.1*heterogeneity + rnorm(sample_size, 0, 1)),
+         opportunity_index = ntile(0.3*disadvantage -0.2*control + rnorm(sample_size, 0, 1), 5),
+         opportunity = case_when(
+           opportunity_index %in% c(1, 2) ~ "low", 
+           opportunity_index %in% c(3, 4) ~ "medium", 
+           opportunity_index == 5 ~ "high"
+           ),
+         robbery = round(1 + exp(-0.25*control + 0.2*disadvantage + 0.05*instability +
+           -0.05*(disadvantage^2) +
+           0.5*(opportunity=="medium") + 1.25*(opportunity=="high") +
+           -0.5*control*(opportunity=="low") -0.15*control*(opportunity=="medium") +
+             rnorm(sample_size, 2, 0.5)), 0)
+         )
+write_csv(neighb_crime, file = "./_data/neighb_crime.csv")
+
+lm_true <- lm(log(robbery) ~ control*opportunity + disadvantage + I(disadvantage^2) + instability + heterogeneity, data = neighb_crime)
+summary(lm_true)
+lm_simple <-lm(robbery ~ control + opportunity + disadvantage + instability + heterogeneity, data = neighb_crime)
+summary(lm_simple)
